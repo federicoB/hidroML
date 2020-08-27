b85d138c0c7ecb0a06892df55b52dd21acbd5587
Federico Bertani <fedbertani@gmail.com>
2020-08-27 18:18:10 +0200

    [WIP] prediction working

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from keras.callbacks import Callback
+from keras.optimizers import SGD
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM, Dropout
 import matplotlib.pyplot as plt
+import os
+from tensorflow.keras import regularizers
 
 
-memory = 27
+memory = 4
 dropout_ratio = 0.2
 training_data_ratio = 0.9
 
@@ -24,9 +28,11 @@ real_discharge = dataset_scaled[train_dataset_limit:]
 
 x_dataset = []
 y_dataset = []
-for i in range(memory, dataset_scaled.shape[0]):
-    x_dataset.append(dataset_scaled[i - memory:i, 0])
-    y_dataset.append(dataset_scaled[i, 0])
+# TODO symplifing going forward
+for i in range(dataset_scaled.shape[0]-memory-1):
+    #TODO check on precision lost
+    x_dataset.append(dataset_scaled[i:i+memory, 0])
+    y_dataset.append(dataset_scaled[i+memory+1, 0])
 x_dataset, y_dataset = np.array(x_dataset), np.array(y_dataset)
 
 # Reshaping
@@ -36,23 +42,38 @@ train_x = x_dataset[:train_dataset_limit + memory]
 train_y = y_dataset[:train_dataset_limit + memory]
 prediction_x = x_dataset[train_dataset_limit+memory:]
 
-regressor = Sequential({
-    LSTM(units=memory, return_sequences=True, input_shape=(1, memory)),
-    Dropout(dropout_ratio),
-    LSTM(units=memory, return_sequences=True),
-    Dropout(dropout_ratio),
-    LSTM(units=memory, return_sequences=True),
-    Dropout(dropout_ratio),
-    LSTM(units=memory),
-    Dropout(dropout_ratio),
-    Dense(1)
-})
+if os.path.exists("model.h5"):
+    regressor = load_model("model.h5")
+else:
+    regressor = Sequential({
+        LSTM(units=memory, return_sequences=False, dropout=dropout_ratio, input_dim=1),
+        Dense(1)
+    })
 
-regressor.compile(optimizer='adam', loss='mean_squared_error')
+    opt = SGD(lr=0.01, momentum=0.9, clipvalue=0.5)
+    regressor.compile(optimizer=opt, loss='mean_absolute_percentage_error')
+    #print(regressor.summary())
 
-regressor.fit(train_x, train_y, epochs=1)
+    print("Shape of training x: {}".format(train_x.shape))
+    print("Shape of training y: {}".format(train_y.shape))
+
+
+    class LossHistory(Callback):
+        def on_train_begin(self, logs={}):
+            self.losses = []
+
+        def on_batch_end(self, batch, logs={}):
+            self.losses.append(logs.get('loss'))
+
+
+    history = LossHistory()
+
+    regressor.fit(train_x, train_y, epochs=0, batch_size=10, callbacks=[history])
+    print(history.losses)
+    #regressor.save("model.h5")
 
 predicted_discharge = regressor.predict(prediction_x)
+predicted_discharge = sc.inverse_transform(predicted_discharge)
 
 # Visualising the results
 plt.plot(real_discharge, color = 'red', label = 'Real discharge')
