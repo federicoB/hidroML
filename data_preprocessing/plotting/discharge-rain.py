f089e5f31e32c53ea4fdad88d7cbb7f99e8ea666
Federico Bertani <fedbertani@gmail.com>
2020-10-08 15:12:29 +0200

    updated discharge-rain plotting

--- data_preprocessing/plotting/discharge-rain.py
+++ data_preprocessing/plotting/discharge-rain.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-input_file_discharge = "data/compiano-discharge-2014-edit.csv"
-input_file_rain = "data/ramiseto-rain-2014-edit.csv"
+basin = "enza"
+river_station = "compiano"
+rain_station = "vetto"
+start_year = 2013
+end_year = 2016
+
+input_file_discharge = "data/discharge/{}/{}/{}-{}.csv".format(basin, river_station, start_year, end_year)
+input_file_rain = "data/rain/{}/{}/{}-{}.csv".format(basin,rain_station,start_year,end_year)
 
 df_discharge = pd.read_csv(input_file_discharge, parse_dates=[0], index_col=0)
 df_rain = pd.read_csv(input_file_rain, parse_dates=[0], index_col=0)
 
 df = pd.concat([df_discharge,df_rain],axis=1)
 
-#df['Dischange'] = df['Dischange'].rolling(48,center=True).mean()
-#df['Rain'] = df['Rain'].rolling(48*14,center=True).mean()
+df['Discharge'] = df['Discharge'].rolling(48,center=True).mean()
+df['Rain'] = df['Rain'].rolling(48*14,center=True).mean()
 
 fig, axs = plt.subplots(2, figsize=(2 ^ 16, 2 ^ 10))
 
 color = 'tab:green'
 axs[0].set_xlabel('date')
-axs[0].set_ylabel('discharge m^3/s', color=color)
+axs[0].set_ylabel('discharme m^3/s', color=color)
 axs[0].plot(df.index, df['Discharge'], color=color)
 axs[0].tick_params(axis='y', labelcolor=color)
 
