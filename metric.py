eb3daf0c406891d2487c498b37917e5cd8bbc1cc
Federico Bertani <fedbertani@gmail.com>
2021-03-17 21:47:34 +0100

    some file renaming

--- metric.py
+++
@@ -1,4 +0,0 @@
-import keras.backend as K
-
-def sum_max_absolute_error(y_true, y_pred):
-    return K.sum(K.max(K.abs(y_pred - y_true), axis=0))
--- transformer/grid_search_log/reporting_grid_search_1.py
+++
@@ -1,9 +0,0 @@
-import talos
-import matplotlib.pyplot as plt
-
-analyze_object = talos.Analyze('general_hyperparameter.csv')
-
-
-analyze_object.plot_bars('d_k','val_max_absolute_error','d_v','ff_dim')
-
-plt.show()
